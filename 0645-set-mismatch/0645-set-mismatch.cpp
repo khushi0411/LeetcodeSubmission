93f9897c10993c6692ee36987fc54f4171268b9e
Radha Yadav <64940343+khushi0411@users.noreply.github.com>
2022-10-24 09:31:07 +0530

    Time: 55 ms (75.67%), Space: 21.8 MB (48.76%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,28 +2,36 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         
-        int n = nums.size();
+        int n = nums.size(); 
+        // size of vector
         vector<char>freq(n+1);
+        // to store the freq/count of the number 
+        // and we are stroring it in vector bcoz we want to return the ans in vector.
         for(int num: nums)
+            // count the freq of nums
             ++freq[num];
         
         int dup = -1;
+        // initialize duplicate
         int mis = -1;
+        // initiliaze missing no
         for(int i =1;i <=n;i++)
+            // triversing whole vector 
         {
-            if(freq[i] == 2)
+            if(freq[i] == 2)  
+                // if count ==2 then it represent that it is repeatedly present in vector that is duplicate
             {
                 dup = i;
             }
                 
-        
+        // if count==0 then it is not present in vector , hence this no. is missing no.
          else if(freq[i] == 0)
            {
                 mis = i;
             }
         
         }
- 
+ // return the vector of duplicate and missing
         return {dup,mis};
         
     }
